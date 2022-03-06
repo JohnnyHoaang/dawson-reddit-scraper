15457a8531daaef79498491b7a9c403dfbc9834f
Strange4 <83730323+Strange4@users.noreply.github.com>
2022-03-05 19:30:47 -0500

    small changes to the sql file

--- Setup.sql
+++ Setup.sql
@@ -2,13 +2,13 @@ DROP TABLE COURSES_TERMS CASCADE CONSTRAINTS;
 DROP TABLE COURSES CASCADE CONSTRAINTS;
 DROP TABLE TERMS CASCADE CONSTRAINTS;
 CREATE TABLE COURSES (
-    course_number VARCHAR2(100) PRIMARY KEY, 
+    course_number CHAR(10) PRIMARY KEY,
     course_name VARCHAR2(100),
     description VARCHAR2(1000),
-    total_hours NUMBER(3),
     class_hours NUMBER(2),
     lab_hours NUMBER(2),
-    homework_hours NUMBER(2)
+    homework_hours NUMBER(2),
+    total_hours NUMBER(3)
 );
 
 CREATE TABLE TERMS (
@@ -17,13 +17,11 @@ CREATE TABLE TERMS (
 
 CREATE TABLE COURSES_TERMS (
     term_number NUMBER(2),
-    course_number VARCHAR2(100) PRIMARY KEY,
-    CONSTRAINT term_fk
-    FOREIGN KEY (term_number)
-    REFERENCES terms (term_number),
-    CONSTRAINT course_fk
-    FOREIGN KEY (course_number)
-    REFERENCES courses (course_number)
+    course_number CHAR(10),
+    CONSTRAINT term_fk FOREIGN KEY (term_number) REFERENCES terms (term_number),
+    CONSTRAINT course_fk FOREIGN KEY (course_number) REFERENCES courses (course_number),
+    CONSTRAINT pk_courses_terms PRIMARY KEY (term_number, course_number)
+
 );
 
 insert into terms VALUES(1);
