08e7c3286d87ea03a9c5ed98cceb710605a7df8d
Strange4 <83730323+Strange4@users.noreply.github.com>
2022-03-08 15:10:03 -0500

    reformated the code to use the query params

--- scrapers/reddit_scraper.py
+++ scrapers/reddit_scraper.py
@@ -6,22 +6,22 @@ import json
 class RedditScraper:
     __url = "https://api.pushshift.io/reddit/search/submission/?subreddit=Dawson"
 
-    def __request(self, query_params: list[str]):
-        req = self.__url + '&'
-        for p in query_params:
-            req += p + '&'
-        page = requests.get(req[0:-1])
+    def __request(self, query_params: dict):
+        page = requests.get(self.__url, query_params)
         if page.status_code != 200:
             raise InvalidCodeException("The page status is not 200")
         return json.loads(page.content)['data']
 
-    def search(self, keyword: str):
-        return self.__request([f'q={keyword}'])
+    def search(self, keyword: list[str]):
+        param = ""
+        for key in keyword:
+            param += key + ','
+        return self.__request({'q': param[0:-1]})
 
 
 if __name__ == '__main__':
     s = RedditScraper()
-    data = s.search('admission')
+    data = s.search(['admission'])
     for d in data:
         print(f"Title: {d['title']}")
         print(f"Body: {d['selftext']}")
